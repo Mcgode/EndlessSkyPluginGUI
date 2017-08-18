ced4b102f9fa19c88c70737c9780502092409726
max <max@godefroy.net>
2017-08-18 12:06:53 +0100

    First main window data element style

--- app/js/main.js
+++ app/js/main.js
@@ -56,9 +56,54 @@ function drawElements(category) {
 
 function drawElementData(category, element) {
     html_utils.empty(element_data_div);
-    let text = game_data.print(category, element);
-    element_data_div.append('p').node().innerText = text.replace(/\t/g, '    ');
-    console.log(text);
+    let obj = game_data.get(category, element);
+    let table = element_data_div.append('table');
+    drawContent(table, obj.content, 0, ['content']);
+}
+
+function drawContent(table, content, indent, keys) {
+    let elements = Object.keys(content);
+    console.log(content);
+    for (let key of elements) {
+        if (content[key] instanceof Array) {
+            content[key].forEach((e, i) => {
+                let row = table.append('tr').attr('class', `row-${indent}`);
+                row.append('p').text(magnify(key)).attr('class', `row-label-${indent}`);
+                e.parameters.forEach((param, j) => {
+                    row.append('input')
+                        .attr('type', 'text').attr('value', param)
+                        .attr('id', `select${stringifyArray([].concat.apply([], [keys, key, i, 'parameters', j]))}`);
+                });
+                if (e.content) {
+                    drawContent(table, e.content, indent + 1, [].concat.apply([], [keys, key, i, 'content']))
+                }
+            });
+        } else {
+            let row = table.append('tr').attr('class', `row-${indent}`);
+            row.append('p').text(magnify(key)).attr('class', `row-label-${indent}`);
+            console.log(key);
+            content[key].parameters.forEach((param, i) => {
+                row.append('input')
+                    .attr('type', 'text').attr('value', param)
+                    .attr('id', `select${stringifyArray([].concat.apply([], [keys, key, 'parameters', i]))}`);
+            });
+            if (content[key].content != null) {
+                drawContent(table, content[key].content, indent + 1, [].concat.apply([], [keys, key, 'content']))
+            }
+        }
+    }
+}
+
+function magnify(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function stringifyArray(array) {
+    let string = '';
+    array.forEach((e, i) => {
+        string += `-${e}`
+    });
+    return string
 }
 
 
@@ -70,7 +115,7 @@ ipcRenderer.on('collect-data', (_, data) => {
     ipcRenderer.send('main-good-to-go');
 });
 
-category_select.on('change', (e) => {
+category_select.on('change', () => {
     current_category = category_select.node().value;
     drawElements(current_category)
 });
